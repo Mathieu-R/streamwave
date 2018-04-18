73cb1b9ff898754cbd247b1a0208af0d641890b8
Mathieu-R <mathieu0709@gmail.com>
2018-04-18 09:45:09 +0200

    [FIX] service worker updating

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { h, render } from 'preact';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import installServiceWorker from './install-service-worker';
 import Constants from './constants';
 import store from './store';
 
@@ -21,56 +22,7 @@ if (process.env.NODE_ENV === 'development') {
   require('preact/debug');
 }
 
-if (Constants.PRODUCTION && Constants.SUPPORT_SERVICE_WORKER) {
-  navigator.serviceWorker.register('/sw.js', {scope: '/'}).then(registration => {
-    if (!registration.active) {
-      return;
-    }
-
-    const isReloading = false;
-    navigator.serviceWorker.oncontrollerchange = evt => {
-      if (isReloading) {
-        return;
-      }
-
-      // refresh the page
-      window.location.reload();
-      isReloading = true;
-    }
-
-    registration.onupdatefound = event => {
-      console.log('A new service worker has been found, installing...');
-    }
-
-    registration.installing.onstatechange = event => {
-      // first time service-worker installed.
-      if (event.target.state === 'activated' && !navigator.serviceWorker.controller) {
-        toasting(['Streamwave cached.', 'Ready to work offline.'])
-        return;
-      }
-
-      // as the service worker is installed, we can push the message
-      if (event.target.state === 'activated') {
-        toasting(['Streamwave updated.', 'Refresh to get the new version.']);
-      }
-
-      console.log(`Service Worker ${event.target.state}`);
-    }
-
-    navigator.serviceWorker.onmessage = event => {
-      if (event.data.type === 'downloading') {
-        const {tracklistId, downloaded, totalDownload} = event.data;
-        store.dispatch(setDownloadPercentage({id: tracklistId, percentage: (downloaded / totalDownload)}));
-        return;
-      }
-
-      if (event.data.type === 'downloaded') {
-        const {tracklistId} = event.data;
-        store.dispatch(removeDownloadPercentage({id: tracklistId}));
-      }
-    }
-  }).catch(err => console.error(err));
-}
+installServiceWorker();
 
 const Main = () => (
   <Provider store={store}>
