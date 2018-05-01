01626bd961c99c038db5261ec9ba8f6fd8d848b3
Mathieu-R <mathieu0709@gmail.com>
2018-05-01 08:55:39 +0200

    fix

--- src/utils/cache.js
+++ src/utils/cache.js
@@ -1,6 +1,4 @@
 import { getConfiguredCache } from 'money-clip';
-// import { get as getIdbKeyVal, set as setIdbKeyVal } from 'idb-keyval';
-// import store from '../store';
 import Constants from '../constants';
 
 const cache = getConfiguredCache({
@@ -8,9 +6,4 @@ const cache = getConfiguredCache({
   maxAge: 365 * 24 * 60 * 60 * 1000 // 1 year
 });
 
-// const userId = store.getState().user.id;
-
-// export const get = key => getIdbKeyVal(`${key}_${userId}`);
-// export const set = (key, item) => setIdbKeyVal(`${key}_${userId}`);
-
-// export default cache;
+export default cache;
