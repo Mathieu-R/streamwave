b24f6f8532390d76e43aca0ea78f5b27b0819736
Mathieu-R <mathieu0709@gmail.com>
2018-05-23 13:03:19 +0200

    fix devserver url path

--- config.js
+++ config.js
@@ -8,6 +8,7 @@ module.exports = {
     entry: {
         front: [path.resolve(__dirname, 'src/index.js')], // entrypoint for front js file
     },
+    contentBase: path.resolve(__dirname, 'src'), // url path for static files
     devtool: production ? false : 'eval-cheap-module-source-map',
     componentsPath: path.resolve(__dirname, 'src/components'), // path for components (aliases)
     routesPath: path.resolve(__dirname, 'src/routes'),
